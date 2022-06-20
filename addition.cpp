95d757d6673a960fa163d3e431353a8cc09d0f63
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-06-20 17:09:14 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+// can change any intiger value and output wiil be changed 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i= 10;
+    char c = 'a';
+
+    int o = i + c;
+    char d = i + c;
+    cout << o << endl;
+    cout << d << endl;
+}
+// output is 
+107
+k
