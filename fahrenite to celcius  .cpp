c0a4081668db09bc7ef758e26b0f9d4cce1b041f
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-06-22 14:33:31 +0530

    Add files via upload

---
+++ fahrenite to celcius  .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int f;
+
+    cout << "Enter the fah value" << endl;
+    cin >> f;
+    int c = (5.0 / 9) * ( f - 32);
+cout << c;
+    
+}
