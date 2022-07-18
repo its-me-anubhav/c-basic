b6da14090506b09684fba78d027668b9e945d9dc
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-07-18 21:39:27 +0530

    Add files via upload

---
+++ Bitwise_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int rand = 15 & 30;
+int rror = 15 | 30;
+int rnot = ~ 30;
+int rxor = 15 ^ 30;
+int rls  = 15 << 2;
+int rrs = 15 >> 2;
+cout << rand << endl;
+cout << rror << endl;
+cout << rnot << endl;
+ cout << rxor << endl;
+ cout << rls << endl;
+ cout << rrs << endl;
+}
